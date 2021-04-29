707d92b208f9c8c117a0a2ce4d4d214ad6483f45
Douglas Adams <douglas.adams@me.com>
2021-04-29 10:34:28 -0400

    v0.3.4

--- Sources/xLib6001/Networking/GuiClient.swift
+++ Sources/xLib6001/Networking/GuiClient.swift
@@ -7,63 +7,47 @@
 
 import Foundation
 
-public struct GuiClient {
-  // ----------------------------------------------------------------------------
-  // MARK: - Public properties
+public class GuiClient: ObservableObject, Identifiable {
+    // ----------------------------------------------------------------------------
+    // MARK: - Published properties
 
-  public var clientId : String? {
-    get { Api.objectQ.sync { _clientId } }
-    set { if newValue != clientId { Api.objectQ.sync(flags: .barrier) { _clientId = newValue }}}}
-  public var handle : Handle {
-    get { Api.objectQ.sync { _handle } }
-    set { if newValue != handle { Api.objectQ.sync(flags: .barrier) { _handle = newValue }}}}
-  public var host : String {
-    get { Api.objectQ.sync { _host } }
-    set { if newValue != host { Api.objectQ.sync(flags: .barrier) { _host = newValue }}}}
-  public var ip : String {
-    get { Api.objectQ.sync { _ip } }
-    set { if newValue != ip { Api.objectQ.sync(flags: .barrier) { _ip = newValue }}}}
-  public var isLocalPtt : Bool {
-    get { Api.objectQ.sync { _isLocalPtt } }
-    set { if newValue != isLocalPtt { Api.objectQ.sync(flags: .barrier) { _isLocalPtt = newValue }}}}
-  public var isThisClient : Bool {
-    get { Api.objectQ.sync { _isThisClient } }
-    set { if newValue != isThisClient { Api.objectQ.sync(flags: .barrier) { _isThisClient = newValue }}}}
-  public var program : String {
-    get { Api.objectQ.sync { _program } }
-    set { if newValue != program { Api.objectQ.sync(flags: .barrier) { _program = newValue }}}}
-  public var station : String {
-    get { Api.objectQ.sync { _station } }
-    set { if newValue != station {  Api.objectQ.sync(flags: .barrier) { _station = newValue }}}}
-  
-  // ----------------------------------------------------------------------------
-  // MARK: - Initialization
-  
-  /// Initialize a GuiClient
-  /// - Parameters:
-  ///   - radio:        the Radio instance
-  ///   - id:           a Tnf Id
-  ///
-  public init(handle: Handle, station: String, program: String, clientId: String? = nil, host: String = "", ip: String = "", isLocalPtt: Bool = false, isThisClient: Bool = false) {
-    _handle = handle
-    _station = station
-    _program = program
-    _clientId = clientId
-    _host = host
-    _ip = ip
-    _isLocalPtt = isLocalPtt
-    _isThisClient = isThisClient
-  }
-  
-  // ----------------------------------------------------------------------------
-  // *** Backing properties (Do NOT use) ***
-  
-  private var _clientId      : String? = nil
-  private var _handle        : Handle = 0
-  private var _host          = ""
-  private var _ip            = ""
-  private var _isLocalPtt    = false
-  private var _isThisClient  = false
-  private var _program       = ""
-  private var _station       = ""
+    @Published public var clientId: String?
+    @Published public var handle: Handle = 0
+    @Published public var host = ""
+    @Published public var ip = ""
+    @Published public var isLocalPtt = false
+    @Published public var isThisClient = false
+    @Published public var program = ""
+    @Published public var station = ""
+
+    // ----------------------------------------------------------------------------
+    // MARK: - Initialization
+
+    /// Initialize a GuiClient
+    /// - Parameters:
+    ///   - radio:        the Radio instance
+    ///   - id:           a Tnf Id
+    ///
+    public init(handle: Handle, station: String, program: String, clientId: String? = nil, host: String = "", ip: String = "", isLocalPtt: Bool = false, isThisClient: Bool = false) {
+        self.handle = handle
+        self.station = station
+        self.program = program
+        self.clientId = clientId
+        self.host = host
+        self.ip = ip
+        self.isLocalPtt = isLocalPtt
+        self.isThisClient = isThisClient
+    }
+    //
+    //  // ----------------------------------------------------------------------------
+    //  // *** Backing properties (Do NOT use) ***
+    //
+    //  private var _clientId      : String? = nil
+    //  private var _handle        : Handle = 0
+    //  private var _host          = ""
+    //  private var _ip            = ""
+    //  private var _isLocalPtt    = false
+    //  private var _isThisClient  = false
+    //  private var _program       = ""
+    //  private var _station       = ""
 }
